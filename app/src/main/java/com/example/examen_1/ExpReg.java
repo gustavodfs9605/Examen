33b436f9a41410b93ad3b8d4b2cf422ff15481a3
gustavodfs9605 <gustavodfs9605@gmail.com>
2020-10-04 16:23:14 -0500

    Ya termine

--- app/src/main/java/com/example/examen_1/ExpReg.java
+++ app/src/main/java/com/example/examen_1/ExpReg.java
@@ -1,10 +1,11 @@
 package com.example.examen_1;
  public class ExpReg {
     static public boolean validarString(String string) {
-        return string.matches("[A-za-a _0-9]{1,15}");
+        return string.matches("[A-za-a _0-9ÑñáéíóúÁÉÍÓÚ]{1,15}");
     }
 
     static public boolean validarFloat(String string) {
         return string.matches("^\\d{1,3}(\\.?\\d)*(,\\d{1,2})?$");
     }
 }
+
